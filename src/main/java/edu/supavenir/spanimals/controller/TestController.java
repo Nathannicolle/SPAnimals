1f2ef78b31a6d209e29b1d52892f95de90337288
papi-luc <luc.papillon@sts-sio-caen.info>
2021-10-22 08:50:23 +0200

    ajout fonction modif

--- src/main/java/edu/supavenir/spanimals/controller/TestController.java
+++ src/main/java/edu/supavenir/spanimals/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 import edu.supavenir.spanimals.model.Joursemaine;
 import edu.supavenir.spanimals.model.Refuge;
@@ -31,12 +32,11 @@ public class TestController {
 		return "formRefuge";
 	}
 
-	@PostMapping("/refugeAdd")
-	public @ResponseBody String addAction(Refuge refuge) {
-		Refuge refuge2 = new Refuge();
-		refugeRepo.saveAndFlush(refuge);
-		return "refuge ajouté :" + refuge;
-	}
+	/*
+	 * @PostMapping("/refugeAdd") public @ResponseBody String addAction(Refuge
+	 * refuge) { Refuge refuge2 = new Refuge(); refugeRepo.saveAndFlush(refuge);
+	 * return "refuge ajouté :" + refuge; }
+	 */
 
 	@GetMapping("/jour")
 
@@ -56,7 +56,13 @@ public class TestController {
 //		return "refuge";
 //	}
 
-	@GetMapping("/modifier/{id}")
+	@PostMapping("/refugeAdd")
+	public RedirectView addAction(Refuge refuge) {
+		refugeRepo.saveAndFlush(refuge);
+		return new RedirectView("/");
+	}
+
+	@GetMapping("/modifRefuge/{id}")
 	public String formModify(@PathVariable String id) {
 		return "formRefuge";
 	}

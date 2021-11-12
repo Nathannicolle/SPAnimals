bc63354a933074dc0092208ff2120393906d5115
Nathannicolle <nathan-nicolle@orange.fr>
2021-11-12 16:22:13 +0100

    add fiche d'un refuges + animal au panel admin

--- src/main/java/edu/supavenir/spanimals/controller/AdminControlleur.java
+++ src/main/java/edu/supavenir/spanimals/controller/AdminControlleur.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.supavenir.spanimals.model.Animal;
+import edu.supavenir.spanimals.model.Refuge;
 import edu.supavenir.spanimals.repositories.AnimalRepository;
 import edu.supavenir.spanimals.repositories.RefugeRepository;
 import io.github.jeemv.springboot.vuejs.VueJS;
@@ -33,13 +36,28 @@ public class AdminControlleur {
 		vue.addData("refuges", refugeRepo.findAll());
 		return "listRefugeAdmin";
 	}
+	
+	@GetMapping("/refuge/{id}")
+	public String indexAction(@PathVariable Integer id) {
+		Refuge refuge = refugeRepo.getById(id);
+		vue.addData("refuge", refugeRepo.getById(id));
+		vue.addData("horaires", refuge.getHoraire());
+		return "refugeAdmin";
+	}
 
 	@GetMapping("/listAnimal")
 	public String getAnimalList() {
 		vue.addData("animals", animalRepo.findAll());
 		vue.addMethod("deleteAnimal", Http.delete("'/rest/animal/'+animal.id", "console.log('supprimé')"), "animal");
 		return "listAnimalAdmin";
-
 	}
-
+	
+	@GetMapping("/animal/{id}")
+	public String getAnimal(@PathVariable Integer id) {
+		Animal animal = animalRepo.getById(id);
+		vue.addData("animal", animalRepo.getById(id));
+		vue.addData("race", animal.getRace());
+		vue.addData("refuge", animal.getRefuge());
+		return "animalAdmin";
+	}
 }

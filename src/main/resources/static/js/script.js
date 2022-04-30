b38caa730aa329740812bed9ba8bf7aa331696e0
Nathannicolle <nathan-nicolle@orange.fr>
2022-04-30 12:56:23 +0200

    working search bar for animals
    fixed issue with search bar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,16 +1,13 @@
 window.addEventListener("load", function(event) {
 	document.querySelector(".search_bar").addEventListener("keyup", () => {
 		document.querySelectorAll(".card_animal").forEach((element) => {
-			document.querySelectorAll(".animal_name").forEach((animal_name) => {
-				console.log(animal_name.innerHTML.toLowerCase().includes(document.querySelector(".search_bar").value.toLowerCase()));
-				if(animal_name.innerHTML.toLowerCase().includes(document.querySelector(".search_bar").value.toLowerCase())) {
-					console.log("find");
-					element.style = "display: flex !important;";
-				} else {
-					console.log("unfind");
-					element.style = "display: none !important;";
-				}
-			});
+			if(element.innerHTML.toLowerCase().includes(document.querySelector(".search_bar").value.toLowerCase())) {
+				console.log("find");
+				element.style = "display: flex !important;";
+			} else {
+				console.log("unfind");
+				element.style = "display: none !important;";
+			}
 		});
 	});
 });
